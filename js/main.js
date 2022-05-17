b0bad2b657dcc23e7b76198337796c67daba421d
EnricoPiombini <piombini.enrico@gmail.com>
2022-05-17 16:13:10 +0200

    Alert

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const nomeUtente = prompt('Qual è il tuo nome?');
 console.log(nomeUtente);
+
 const cognomeUtente = prompt('Qual è il tuo cognome?');
 console.log(cognomeUtente);
+
 const colorePreferito = prompt('Qual è il tuo colore preferito?');
 console.log(colorePreferito);
+
+
+alert(`${nomeUtente} ${cognomeUtente} il tuo colore preferito è ${colorePreferito}`);
