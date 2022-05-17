aac12d9a35960505d6a001c97f5d082eb94dbb59
EnricoPiombini <piombini.enrico@gmail.com>
2022-05-17 16:05:17 +0200

    Variabili

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 const nomeUtente = prompt('Qual è il tuo nome?');
+console.log(nomeUtente);
 const cognomeUtente = prompt('Qual è il tuo cognome?');
+console.log(cognomeUtente);
 const colorePreferito = prompt('Qual è il tuo colore preferito?');
-
+console.log(colorePreferito);
